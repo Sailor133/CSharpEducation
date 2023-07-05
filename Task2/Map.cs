757fcf032fadc7c0c4d8e1c0e2fe263ff9255106
Sailor133 <prikolprikolovi4@mail.ru>
2023-07-05 13:12:04 +0400

    Создание скелета
    Создание поля и реализация постановки крестика

---
+++ Task2/Map.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    static class Map
+    {
+
+        private static char[,] map = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+        /// <summary>
+        /// Вывод поля в консоль.
+        /// </summary>
+        public static void DrowMap()
+        {
+            int counter = 1;
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                Console.WriteLine(string.Join("", Enumerable.Repeat("----", map.GetLength(0)))); //Вывод границы строки.
+                Console.Write("| "); //Вывод границы поля.
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(string.Join("", Enumerable.Repeat("----", map.GetLength(0)))); //Вывод границы строки.
+        }
+        /// <summary>
+        /// Добавление на игровое поле знака.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="mark"></param>
+        public static void MakeMove(int value, char choice)
+        {
+            int i = (value - 1) / 3;
+            int j = (value - 1) % 3;
+            map[i, j] = choice;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Первые ходят крестики - X");
+                Console.WriteLine("Выберете клетку для хода");
+                Map.DrowMap();
+                var choice = Console.ReadLine();
+                int intChoice;
+                try
+                {
+                    intChoice = int.Parse(choice);
+                    Map.MakeMove(intChoice, 'X');
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                }
+                
+            }
+        }
+
+
+    }
+}
